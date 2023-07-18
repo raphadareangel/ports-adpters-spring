ade1e92ae30b7661ef6b2bf228681b700d397d07
raphadareangel <raphadareangel@hotmail.com>
2023-07-18 12:23:11 +0200

    commit testing what is in github
    Solved all the issues after commit and download the project from git

--- zara-adapters/target/test-classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into brand (name) values ('puma');
-insert into product (name) values ('tennis');
-insert into pricing (brand_id, start_date, end_date, price_list, priority, price, currency,product_id)
-values (1, '2023-07-01T10:00:00', '2023-07-31T23:59:59', 'Summer Sale', 1, 25.99, 'USD',2);
